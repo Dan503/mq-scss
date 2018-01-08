c2e77485ddc54d7b8e0c6b4ae7780992d16f4899
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-01-08 21:55:25 +1100

    Updated unit tests to expect "not all" instead of "not screen"

--- unit-tests/ratio/exactRatioWorkAround3/test.js
+++ unit-tests/ratio/exactRatioWorkAround3/test.js
@@ -1,3 +1,3 @@
 import unitTest from '../../../gulp/helpers/unitTest';
 
-unitTest('exactRatioWorkAround3', __dirname, '@media not screen and (max-aspect-ratio: 2 / 1){.unitTest{color:#000}}')
+unitTest('exactRatioWorkAround3', __dirname, '@media not all and (max-aspect-ratio: 2 / 1){.unitTest{color:#000}}')
